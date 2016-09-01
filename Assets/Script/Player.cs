054ec3d67e49a3939c38272d3c08f0a72f54ef45
Keiichi Ishii <ishii.keiiti.228@gmail.com>
2016-09-01 16:24:42 +0900

    スプライト修正

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,59 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour
+{
     public float moveValue = 15f;//移動量.
     public float jumppower = 5f;
-
-
-
     public SpriteRenderer render;
     public Sprite jumpImage, walkImage;
 
     private bool isGround;
     private Animator anime;
-    void Start() {
+    void Start()
+    {
         //あらかじめSpriteRenderを取得する
         render = GetComponent<SpriteRenderer>();
         isGround = false;
         anime = GetComponent<Animator>();
-        anime.enabled = false;
+        anime.enabled = true;
         isGround = false;
     }
 
-    
-    void Update() {
+
+    void Update()
+    {
         //→が押されたらYES　それ以外はNO.
-        if (Input.GetKey(KeyCode.RightArrow)) { 
+        //animator.SetBool("tap", false);
+        if (isGround == true)
+        {
+            anime.enabled = true;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
             //                   new Vector2(       x,                   y).
             transform.Translate(new Vector2(moveValue * Time.deltaTime, 0f));
+            anime.SetBool("tap", true);
+            anime.SetBool("right", true);
             if (isGround == true)
             {
-                anime.enabled = true;
+                //anime.enabled = true;
             }
         }
-         else  if (Input.GetKey(KeyCode.LeftArrow))
+        else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //                   new Vector2(       x,                   y).
             transform.Translate(new Vector2(-moveValue * Time.deltaTime, 0f));
-            if (isGround == true)
-            {
-                anime.enabled = true;
-            }
+            anime.SetBool("tap", true);
+            anime.SetBool("right", false);
         }
         //着地しているかつジャンプやりたい
-        if (isGround&&Input.GetKey(KeyCode.Space))
+        if (isGround && Input.GetKey(KeyCode.Space))
         {
             GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumppower * 100f));
-            render.sprite = jumpImage;
+            anime.enabled = false;
             isGround = false;
         }
         if (Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKeyUp(KeyCode.LeftArrow))
         {
-            anime.enabled = false;
+            anime.SetBool("tap", false);
             if (isGround)
             {
-                render.sprite = walkImage;
+                //render.sprite = walkImage;
             }
             render.sprite = jumpImage;
         }
@@ -64,25 +70,26 @@ public class Player : MonoBehaviour {
         if (col.tag == "Ground" || col.tag == "Object")
         {
             isGround = true;
-            render.sprite = walkImage;
+            //render.sprite = walkImage;
         }
         {
             isGround = true;
-            render.sprite = walkImage;
+            //render.sprite = walkImage;
         }
-        if (col.tag == "death"||col.tag=="enemy")
+        if (col.tag == "death" || col.tag == "enemy")
         {
             restart();
         }
-        if(col.tag=="goal")
+        if (col.tag == "goal")
         {
             Invoke("restart", 3f);
             GetComponent<Player>().enabled = false;
             GetComponent<shot>().enabled = false;
             GetComponent<Animator>().enabled = false;
-        }        
+        }
     }
-    void restart() {
+    void restart()
+    {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
